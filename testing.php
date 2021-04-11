ad414245266bb27a31d95f751d3fd6fca7155563
pankaj bisht <bisht.pankaj90@gmail.com>
2021-04-11 16:16:32 +0530

    new file add this folder

---
+++ testing.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Amasty Team is for php developer
+ * @copyright Copyright (c) 2015 Amasty (http://www.amasty.com)
+ * @package Amasty_TextoverImages  tst
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Amasty_TextoverImages',
+    __DIR__
+);
